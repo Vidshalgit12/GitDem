ccc980d3cdaec88fa0e15dd269341e09c8d95fa5
Vidshalgit12 <vishalldh_mehta@yahoo.com>
2024-01-07 22:00:17 +1300

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,18 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hun="yes";
+		if(hun=="yes")
+		{
+			System.out.println ("Eat Som");
+		}
+		else
+		{
+			System.out.println ("Do som Work");
+		}
+
+	}
+
+}
